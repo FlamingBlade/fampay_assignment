8d7589770f73a26ba6e6184c3862fce654b70f12
FlamingBlade <m.abhiram12345@gmail.com>
2024-12-30 00:52:32 +0530

    changed endpoints

--- fampay_assignment/videoSearch/urls.py
+++ fampay_assignment/videoSearch/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('test',views.test),
-    path('yt',views.start_fetching_videos),
+    path('start',views.start_fetching_videos),
     path('fetch',views.get_videos),
     path('stop',views.stop_fetching),
     path('search',views.search_videos),
